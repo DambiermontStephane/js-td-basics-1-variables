df448e5cb7c2ad85a68ed6e4002942f6898e13d4
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2023-10-05 14:40:37 +0200

    commit

--- OCR-starter/chapitre2_exercice2.js
+++ OCR-starter/chapitre2_exercice2.js
@@ -1,3 +1,9 @@
 // Celsius - Fahrenheit
 
+const degreCelsius = prompt('Combien de degrés ?')
+
+const convertionFar = ((degreCelsius * 9 / 5)+32)
+
+console.log(`${degreCelsius}° Celsius vaut ${convertionFar}° Fahrenheit`)
+
 
